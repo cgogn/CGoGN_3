9144345f05c3174def6273dd25dfd36c577896f8
huang qijia <qijiahuang1@gmail.com>
2023-03-21 11:24:47 +0100

    test demo

--- cgogn/modeling/apps/voronoi_medial_mesh.cpp
+++ cgogn/modeling/apps/voronoi_medial_mesh.cpp
@@ -27,76 +27,107 @@
 #include <cgogn/ui/view.h>
 
 #include <cgogn/core/ui_modules/mesh_provider.h>
-#include <cgogn/geometry/ui_modules/surface_differential_properties.h>
-#include <cgogn/rendering/ui_modules/surface_render.h>
+#include <cgogn/rendering/ui_modules/volume_render.h>
 
-#define DEFAULT_MESH_PATH CGOGN_STR(CGOGN_DATA_PATH) "/meshes/"
+#include <cgogn/geometry/algos/centroid.h>
+#include <cgogn/geometry/functions/distance.h>
 
-// using Mesh = cgogn::IncidenceGraph;
-using Mesh = cgogn::CMap2;
+#include <cgogn/modeling/algos/volume_utils.h>
 
+#include <cgogn/core/utils/numerics.h>
+#include <cgogn/io/utils.h>
+#include <cgogn/io/volume/volume_import.h>
+
+#include <CGAL/Delaunay_triangulation_3.h>
+#include <CGAL/Delaunay_triangulation_cell_base_3.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Triangulation_vertex_base_with_info_3.h>
+#include <vector>
+
+using K = CGAL::Exact_predicates_inexact_constructions_kernel;
+using Vb = CGAL::Triangulation_vertex_base_with_info_3<uint32_t, K>;
+using Cb = CGAL::Delaunay_triangulation_cell_base_3<K>;
+using Tds = CGAL::Triangulation_data_structure_3<Vb, Cb>;
+using Delaunay = CGAL::Delaunay_triangulation_3<K, Tds, CGAL::Fast_location>;
+using Point = Delaunay::Point;
+
+using Mesh = cgogn::CMap3;
 template <typename T>
 using Attribute = typename cgogn::mesh_traits<Mesh>::Attribute<T>;
+using namespace cgogn::numerics;
+
 using Vertex = typename cgogn::mesh_traits<Mesh>::Vertex;
-using Face = typename cgogn::mesh_traits<Mesh>::Face;
+using Volume = typename cgogn::mesh_traits<Mesh>::Volume;
 
 using Vec3 = cgogn::geometry::Vec3;
 using Scalar = cgogn::geometry::Scalar;
 
+void test_delaunay(Mesh* m)
+{
+	cgogn::io::VolumeImportData volume_data;
+
+	volume_data.vertex_position_.emplace_back(0, 0, 0);
+	volume_data.vertex_position_.emplace_back(1, 0, 0);
+	volume_data.vertex_position_.emplace_back(0, 1, 0);
+	volume_data.vertex_position_.emplace_back(0, 0, 1);
+	volume_data.vertex_position_.emplace_back(1, 1, 1);
+	volume_data.vertex_position_.emplace_back(-1, 0, 1);
+
+	uint32 nb_vertices = volume_data.vertex_position_.size();
+
+	std::vector<unsigned> indices;
+	indices.reserve(nb_vertices);
+	for (int i = 0; i < nb_vertices; ++i)
+		indices.push_back(i);
+
+	// risky !!
+	std::vector<Point>* points = reinterpret_cast<std::vector<Point>*>(&volume_data.vertex_position_);
+
+	Delaunay tri(boost::make_zip_iterator(boost::make_tuple(points->begin(), indices.begin())),
+				 boost::make_zip_iterator(boost::make_tuple(points->end(), indices.end())));
+
+	uint32 nb_volumes = tri.number_of_finite_cells();
+	volume_data.reserve(nb_vertices, nb_volumes);
+
+	for (auto cit = tri.finite_cells_begin(); cit != tri.finite_cells_end(); ++cit)
+	{
+		volume_data.volumes_types_.push_back(cgogn::io::VolumeType::Tetra);
+		volume_data.volumes_vertex_indices_.push_back(cit->vertex(0)->info());
+		volume_data.volumes_vertex_indices_.push_back(cit->vertex(1)->info());
+		volume_data.volumes_vertex_indices_.push_back(cit->vertex(2)->info());
+		volume_data.volumes_vertex_indices_.push_back(cit->vertex(3)->info());
+	}
+
+	import_volume_data(*m, volume_data);
+
+	// Why ?
+	cgogn::index_cells<Volume>(*m);
+}
+
 int main(int argc, char** argv)
 {
-	std::string filename;
-	if (argc > 1)
-		// 	filename = std::string(DEFAULT_MESH_PATH) + std::string("off/socket.off");
-		// else
-		filename = std::string(argv[1]);
 
 	cgogn::thread_start();
 
 	cgogn::ui::App app;
-	app.set_window_title("Voronoi medial mesh");
-	app.set_window_size(1000 * app.get_dpi_scale(), 800*app.get_dpi_scale());
+	app.set_window_title("Delaunay volume viewer");
+	app.set_window_size(1000, 800);
 
 	cgogn::ui::MeshProvider<Mesh> mp(app);
-	cgogn::ui::SurfaceRender<Mesh> sr(app);
-	cgogn::ui::SurfaceDifferentialProperties<Mesh> sdp(app);
+	cgogn::ui::VolumeRender<Mesh> vr(app);
 
 	app.init_modules();
 
 	cgogn::ui::View* v1 = app.current_view();
 	v1->link_module(&mp);
-	v1->link_module(&sr);
+	v1->link_module(&vr);
 
-	if (filename.length() > 0)
-	{
-		Mesh* m = mp.load_surface_from_file(filename);
-		if (!m)
-		{
-			std::cout << "File could not be loaded" << std::endl;
-			return 1;
-		}
+	Mesh* m = mp.add_mesh("delaunay");
+	test_delaunay(m);
 
-		std::shared_ptr<Attribute<Vec3>> vertex_position = cgogn::get_attribute<Vec3, Vertex>(*m, "position");
-		std::shared_ptr<Attribute<Vec3>> vertex_normal = cgogn::add_attribute<Vec3, Vertex>(*m, "normal");
-
-		// std::shared_ptr<Attribute<Vec3>> face_color = cgogn::add_attribute<Vec3, Face>(*m, "color");
-		// std::shared_ptr<Attribute<Scalar>> face_weight = cgogn::add_attribute<Scalar, Face>(*m, "weight");
-
-		// cgogn::foreach_cell(*m, [&](Face f) -> bool {
-		// 	Vec3 c(0, 0, 0);
-		// 	c[rand() % 3] = 1;
-		// 	cgogn::value<Vec3>(*m, face_color, f) = c;
-		// 	cgogn::value<Scalar>(*m, face_weight, f) = double(rand()) / RAND_MAX;
-		// 	return true;
-		// });
-
-		// mp.set_mesh_bb_vertex_position(*m, vertex_position);
-
-		sdp.compute_normal(*m, vertex_position.get(), vertex_normal.get());
-
-		sr.set_vertex_position(*v1, *m, vertex_position);
-		sr.set_vertex_normal(*v1, *m, vertex_normal);
-	}
+	std::shared_ptr<Attribute<Vec3>> vertex_position = cgogn::get_attribute<Vec3, Vertex>(*m, "position");
+	mp.set_mesh_bb_vertex_position(*m, vertex_position);
+	vr.set_vertex_position(*v1, *m, vertex_position);
 
 	return app.launch();
 }
