a7b52e82530a7bd92497207e06a6dc276ea18c9d
huang qijia <qijiahuang1@gmail.com>
2023-03-23 16:57:34 +0100

    03/23/2023

--- cgogn/geometry/apps/delaunay3_viewer.cpp
+++ cgogn/geometry/apps/delaunay3_viewer.cpp
@@ -28,6 +28,7 @@
 
 #include <cgogn/core/ui_modules/mesh_provider.h>
 #include <cgogn/rendering/ui_modules/volume_render.h>
+#include <cgogn/rendering/ui_modules/surface_render.h>
 
 #include <cgogn/geometry/algos/centroid.h>
 #include <cgogn/geometry/functions/distance.h>
@@ -42,6 +43,10 @@
 #include <CGAL/Delaunay_triangulation_cell_base_3.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Triangulation_vertex_base_with_info_3.h>
+#include <CGAL/Polygon_mesh_processing/distance.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/IO/read_off_points.h>
+
 #include <vector>
 #include <random>
 #include <chrono>
@@ -52,45 +57,44 @@ using Cb = CGAL::Delaunay_triangulation_cell_base_3<K>;
 using Tds = CGAL::Triangulation_data_structure_3<Vb, Cb>;
 using Delaunay = CGAL::Delaunay_triangulation_3<K, Tds, CGAL::Fast_location>;
 using Point = Delaunay::Point;
+using Kernel =  CGAL::Simple_cartesian<double>;
+using Cgal_Surface_mesh = CGAL::Surface_mesh<Kernel::Point_3> ;
 
 #define DEFAULT_MESH_PATH CGOGN_STR(CGOGN_DATA_PATH) "/meshes/"
 
-using Mesh = cgogn::CMap3;
+
+using Mesh_Volumn = cgogn::CMap3;
 template <typename T>
-using Attribute = typename cgogn::mesh_traits<Mesh>::Attribute<T>;
+using Attribute_Volumn = typename cgogn::mesh_traits<Mesh_Volumn>::Attribute<T>;
 using namespace cgogn::numerics;
 
-using Vertex = typename cgogn::mesh_traits<Mesh>::Vertex;
-using Volume = typename cgogn::mesh_traits<Mesh>::Volume;
+using Vertex_Volumn = typename cgogn::mesh_traits<Mesh_Volumn>::Vertex;
+using Volume = typename cgogn::mesh_traits<Mesh_Volumn>::Volume;
 
 using Vec3 = cgogn::geometry::Vec3;
 using Scalar = cgogn::geometry::Scalar;
 
+using Mesh_surface = cgogn::CMap2;
+template <typename T>
+using Attribute_surface = typename cgogn::mesh_traits<Mesh_surface>::Attribute<T>;
+using namespace cgogn::numerics;
+
+using Vertex_surface = typename cgogn::mesh_traits<Mesh_surface>::Vertex;
+
 //#define PERF_TEST
 
-void test_delaunay(Mesh* m)
+void test_delaunay(Mesh_Volumn* m, Cgal_Surface_mesh sm)
 {
 	cgogn::io::VolumeImportData volume_data;
 
-	std::random_device rd;	// Will be used to obtain a seed for the random number engine
-	std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-	std::uniform_real_distribution<double> dis(-1.0, 1.0);
-
-	#ifdef PERF_TEST
-	for (int i = 0; i < 150000; ++i)
+	std::vector<Kernel::Point_3> mesh_samples;
+	CGAL::Polygon_mesh_processing::sample_triangle_mesh(sm, 
+														std::back_inserter(mesh_samples),
+														CGAL::parameters::use_grid_sampling(true).grid_spacing(5));
+	for (auto s : mesh_samples)
 	{
-		Vec3 P(dis(gen), dis(gen), dis(gen));
-		if (P.dot(P) < 1.0)
-			volume_data.vertex_position_.push_back(P);
+		volume_data.vertex_position_.emplace_back(s[0], s[1], s[2]);
 	}
-	#else
-		volume_data.vertex_position_.emplace_back(0, 0, 0);
-		volume_data.vertex_position_.emplace_back(0, 0, 1);
-		volume_data.vertex_position_.emplace_back(0, 0, -1);
-		volume_data.vertex_position_.emplace_back(0, 1, 0);
-		volume_data.vertex_position_.emplace_back(0.866, -0.5 , 0);
-		volume_data.vertex_position_.emplace_back(-0.866,-0.5 ,0);
-	#endif
 
 	uint32 nb_vertices = volume_data.vertex_position_.size();
 
@@ -98,7 +102,7 @@ void test_delaunay(Mesh* m)
 
 	std::vector<unsigned> indices;
 	indices.reserve(nb_vertices);
-	for (int i = 0; i < nb_vertices; ++i)
+	for (int i = 0; i < nb_vertices; ++i) 
 		indices.push_back(i);
 
 	std::vector<Point>* points = reinterpret_cast<std::vector<Point>*>(&volume_data.vertex_position_);
@@ -133,6 +137,19 @@ void test_delaunay(Mesh* m)
 
 int main(int argc, char** argv)
 {
+	std::string filename;
+	if (argc > 1)
+		filename = std::string(argv[1]);
+	
+	//Load surface mesh using CGAL;
+	Cgal_Surface_mesh surface_mesh;
+	
+	std::ifstream input(filename);
+	if (!input || !(input >> surface_mesh))
+	{
+		std::cerr << "Error: input OFF file could not be read" << std::endl;
+		return EXIT_FAILURE;
+	}
 
 	cgogn::thread_start();
 
@@ -140,21 +157,41 @@ int main(int argc, char** argv)
 	app.set_window_title("Delaunay volume viewer");
 	app.set_window_size(1000, 800);
 
-	cgogn::ui::MeshProvider<Mesh> mp(app);
-	cgogn::ui::VolumeRender<Mesh> vr(app);
+	cgogn::ui::MeshProvider<Mesh_Volumn> mp(app);
+	cgogn::ui::MeshProvider<Mesh_surface> mps(app);
 
+	cgogn::ui::VolumeRender<Mesh_Volumn> vr(app);
+	cgogn::ui::SurfaceRender<Mesh_surface> sr(app);
 	app.init_modules();
 
 	cgogn::ui::View* v1 = app.current_view();
 	v1->link_module(&mp);
 	v1->link_module(&vr);
+	v1->link_module(&mps);
+	v1->link_module(&sr);
 
-	Mesh* m = new Mesh{};
-	test_delaunay(m);
-	mp.register_mesh(m,"delaunay");
-	std::shared_ptr<Attribute<Vec3>> vertex_position = cgogn::get_attribute<Vec3, Vertex>(*m, "position");
+
+ 	Mesh_Volumn* m = new Mesh_Volumn{};
+	test_delaunay(m, surface_mesh);
+	mp.register_mesh(m, "delaunay");
+	std::shared_ptr<Attribute_Volumn<Vec3>> vertex_position = cgogn::get_attribute<Vec3, Vertex_Volumn>(*m, "position");
 	mp.set_mesh_bb_vertex_position(*m, vertex_position);
 	vr.set_vertex_position(*v1, *m, vertex_position);
+
+
+	//Load surface mesh using CGoGn
+	Mesh_surface* original_m = mps.load_surface_from_file(filename);
+	if (!original_m)
+	{
+		std::cout << "File could not be loaded" << std::endl;
+		return 1;
+	}
+
+	std::shared_ptr<Attribute_surface<Vec3>> vertex_position_2 = cgogn::get_attribute<Vec3, Vertex_surface>(*original_m, "position");
+	std::shared_ptr<Attribute_surface<Vec3>> vertex_normal_2 = cgogn::add_attribute<Vec3, Vertex_surface>(*original_m, "normal");
+
+	sr.set_vertex_position(*v1, *original_m, vertex_position_2);
+	sr.set_vertex_normal(*v1, *original_m, vertex_normal_2);
 
 	return app.launch();
 }
