56d86f388d3cbfb7a8a1d823a609dd115b549452
huang qijia <qijiahuang1@gmail.com>
2025-09-09 15:59:51 +0200

    revert a local change of range in the node vector specified for Windows

--- thirdparty/libacc/bvh_tree.h
+++ thirdparty/libacc/bvh_tree.h
@@ -365,7 +365,7 @@ std::pair<typename BVHTree<IdxType, Vec3fType>::Node::ID, typename BVHTree<IdxTy
 	std::vector<AABB> right_aabbs(n);
 	for (char d = 0; d < 3; ++d)
 	{
-		std::sort(&indices[node.first], &indices[node.last-1], [&aabbs, d](IdxType first, IdxType second) -> bool {
+		std::sort(&indices[node.first], &indices[node.last], [&aabbs, d](IdxType first, IdxType second) -> bool {
 			return mid(aabbs[first], d) < mid(aabbs[second], d) ||
 				   (mid(aabbs[first], d) == mid(aabbs[second], d) && first < second);
 		});
@@ -400,7 +400,7 @@ std::pair<typename BVHTree<IdxType, Vec3fType>::Node::ID, typename BVHTree<IdxTy
 	char d;
 	IdxType i;
 	std::tie(d, i) = split;
-	std::sort(&indices[node.first], &indices[node.last-1], [&aabbs, d](std::size_t first, std::size_t second) -> bool {
+	std::sort(&indices[node.first], &indices[node.last], [&aabbs, d](std::size_t first, std::size_t second) -> bool {
 		return mid(aabbs[first], d) < mid(aabbs[second], d) ||
 			   (mid(aabbs[first], d) == mid(aabbs[second], d) && first < second);
 	});
